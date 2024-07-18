2bc8c609f5896abd71d3fbe54c15fb8d9c9a2a4a
tuyenxngoc <tuyenn9448@gmail.com>
2024-07-18 17:58:13 +0700

    feat: Add scheduled task for cleaning up unverified users
    - Implement UnverifiedUserCleanupTask to remove unverified users older than 24 hours.
    - Schedule task to run every 24 hours using @Scheduled annotation.

--- src/main/java/com/tuyenngoc/army2forum/repository/UserRepository.java
+++ src/main/java/com/tuyenngoc/army2forum/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.tuyenngoc.army2forum.repository;
 
 import com.tuyenngoc.army2forum.domain.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Repository
@@ -20,4 +23,9 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsernameAndEmail(String username, String email);
 
     Optional<User> findByVerificationCode(String token);
+
+    @Modifying
+    @Transactional
+    void deleteAllByEnabledFalseAndCreatedDateBefore(LocalDateTime dateTime);
+
 }
---
+++ src/main/java/com/tuyenngoc/army2forum/task/UnverifiedUserCleanupTask.java
@@ -0,0 +1,21 @@
+package com.tuyenngoc.army2forum.task;
+
+import com.tuyenngoc.army2forum.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class UnverifiedUserCleanupTask {
+
+    private final UserRepository userRepository;
+
+    @Scheduled(fixedRate = 86400000) // 24 hours in milliseconds
+    public void cleanupUnverifiedUsers() {
+        userRepository.deleteAllByEnabledFalseAndCreatedDateBefore(LocalDateTime.now().minusHours(24));
+    }
+
+}
